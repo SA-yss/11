a35809a912006a8d255f9464c0e3c4ce463df1c2
SA <yss@seoulauction.com>
2026-04-26 12:58:20 +0900

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-/* config.js - OneSignal 초기화 */
-
-window.OneSignalDeferred = window.OneSignalDeferred || [];
-    OneSignalDeferred.push(async function(OneSignal) {
-      await OneSignal.init({
-        appId: "ef0e8e78-9ec6-4e92-8bed-9f1eff07b903",
-      });
-    });

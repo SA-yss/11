6df19fcc54676d2ab5d37f68768adba52d62564d
SA <yss@seoulauction.com>
2026-04-26 11:54:44 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
-/* config.js - 외부 초기화 설정 */
+/* config.js - OneSignal 초기화 */
 
-// OneSignal 초기화
 window.OneSignalDeferred = window.OneSignalDeferred || [];
-OneSignalDeferred.push(async function(OneSignal) {
-  await OneSignal.init({
-    appId: "ef0e8e78-9ec6-4e92-8bed-9f1eff07b903",
-  });
-});
+    OneSignalDeferred.push(async function(OneSignal) {
+      await OneSignal.init({
+        appId: "ef0e8e78-9ec6-4e92-8bed-9f1eff07b903",
+      });
+    });
